2292f6a9bfa22512fd9b79e757fae414ad574cbd
Nitni <nitinit077@gmail.com>
2013-12-09 01:19:47 +0530

    Revert "Revert "Revert "Revert "Base structure""""
    This reverts commit 0b50b0aaf013565790271116df4d4e726302a2af.

---
+++ Base/Front end/Base classes/CFViewController.m
@@ -0,0 +1,38 @@
+//
+//  CFViewController.m
+//  Base
+//
+//  Created by Manish Shah on 08/12/13.
+//  Copyright (c) 2013 conficle. All rights reserved.
+//
+
+#import "CFViewController.h"
+
+@interface CFViewController ()
+
+@end
+
+@implementation CFViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
