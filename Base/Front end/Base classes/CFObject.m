d42898ff4136a13647a947fe075547a80e9cb43d
Nitni <nitinit077@gmail.com>
2013-12-09 01:13:28 +0530

    Revert "Revert "Base structure""
    This reverts commit 814cf7f75002ca1de888cbed8d025912b8479c6b.

---
+++ Base/Front end/Base classes/CFObject.m
@@ -0,0 +1,13 @@
+//
+//  CFObject.m
+//  Base
+//
+//  Created by Manish Shah on 08/12/13.
+//  Copyright (c) 2013 conficle. All rights reserved.
+//
+
+#import "CFObject.h"
+
+@implementation CFObject
+
+@end
---
+++ Base/Front end/Base classes/CFViewController.h
@@ -0,0 +1,13 @@
+//
+//  CFViewController.h
+//  Base
+//
+//  Created by Manish Shah on 08/12/13.
+//  Copyright (c) 2013 conficle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CFViewController : UIViewController
+
+@end
