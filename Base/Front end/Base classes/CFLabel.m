ea2b7ca2db67859e0823034624773cef5199f7b0
Nitni <nitinit077@gmail.com>
2013-12-09 01:16:43 +0530

    Revert "Revert "Revert "Revert "Base structure""""
    This reverts commit dd75bca0732fd0d53351e78ae831a6bc078a84e3.

---
+++ Base/Front end/Base classes/CFLabel.m
@@ -0,0 +1,31 @@
+//
+//  CFLabel.m
+//  Base
+//
+//  Created by Manish Shah on 08/12/13.
+//  Copyright (c) 2013 conficle. All rights reserved.
+//
+
+#import "CFLabel.h"
+
+@implementation CFLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ Base/Front end/CFAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  CFAppDelegate.h
+//  Base
+//
+//  Created by Manish Shah on 08/12/13.
+//  Copyright (c) 2013 conficle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CFSplashVC;
+
+@interface CFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
