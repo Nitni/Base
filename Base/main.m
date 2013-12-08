9eac59691dabad286098d34408dd9b214bd66cdb
Nitni <nitinit077@gmail.com>
2013-12-09 01:17:19 +0530

    Revert "Revert "Base structure""
    This reverts commit 814cf7f75002ca1de888cbed8d025912b8479c6b.

---
+++ Base/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Base
+//
+//  Created by Manish Shah on 08/12/13.
+//  Copyright (c) 2013 conficle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CFAppDelegate class]));
+    }
+}
